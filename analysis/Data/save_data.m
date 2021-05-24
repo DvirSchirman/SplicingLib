6a0d7d9b24d05763b55b16c429ccbcd8d58ef187
dvisi988 <dvisi988@ICM-DVL96D3.user.uu.se>
2021-05-24 20:42:23 +0200

    update

--- analysis/Data/save_data.m
+++ analysis/Data/save_data.m
@@ -8,18 +8,20 @@ get_GC =@(x) (sum(x=='C')+sum(x=='G'))./length(x);
 get_Y = @(x) (sum(x=='T')+sum(x=='C'))/length(x);
 get_T = @(x) (sum(x=='T'))/length(x);
 
-inds=find(cellfun(@isempty,splicing_lib_tbl.intron_len));
-splicing_lib_tbl(inds,:)=[];
-inds=find(cellfun(@length,splicing_lib_tbl.branch_pos_from_3)~=1);
-splicing_lib_tbl(inds,:)=[];
-inds=find(cellfun(@length,splicing_lib_tbl.W_tail_len)~=1);
-splicing_lib_tbl(inds,:)=[];
+% inds=find(cellfun(@isempty,splicing_lib_tbl.intron_len));
+% splicing_lib_tbl(inds,:)=[];
+% inds=find(cellfun(@length,splicing_lib_tbl.branch_pos_from_3)~=1);
+% splicing_lib_tbl(inds,:)=[];
+% inds=find(cellfun(@length,splicing_lib_tbl.W_tail_len)~=1);
+% splicing_lib_tbl(inds,:)=[];
 
 splicing_lib_tbl.intron_seq=cellfun(@(x,y,z) x(y(1)+42:z(end)+42),splicing_lib_tbl.seq,splicing_lib_tbl.SS5_inds,splicing_lib_tbl.SS3_inds,'un',0);
 splicing_lib_tbl.intron_len = cell2mat(splicing_lib_tbl.intron_len);
 splicing_lib_tbl.branch_pos_from_3 = cell2mat(splicing_lib_tbl.branch_pos_from_3);
 splicing_lib_tbl.W_tail_len = cell2mat(splicing_lib_tbl.W_tail_len);
 splicing_lib_tbl.polyT=cellfun(@(x) get_T(x(end-19:end)), splicing_lib_tbl.intron_seq);
+
+%%
 
 splicing_lib_tbl(:,[6,7,8,10,19,20,21,27,30,33,38,39,40,41,42,44,52:58, 60, 61])=[];
 tmp = splicing_lib_tbl.Properties.VariableNames;
